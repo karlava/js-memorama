ab7f584b9dfad43c3ab6b25858c3aa9b7c7261cf
rodolfo010385 <rodolfo010385@54d1e357-2446-0410-accf-077c31a16dfc>
2008-02-19 23:39:10 +0000

    se quito el depliegue de mensajes

--- creaElementoTexto.js
+++ creaElementoTexto.js
@@ -2,12 +2,8 @@
 	transformaCadena - proyecto
 	funcion que crea un nodo elemento "img" a un nodo padre que recive como parametro.
 	parametros:
-		IdentificadorPadre: Es el identificador del nodo padre al que se le creara un nodo hijo de tipo "tipoElemento".
-		identificador:Es el identificador por medio del cual se podra reconocer al nodo "img" que se va a crear.(esto 
-		por lo que habiamos quedado de que ibamos a identificar las imagenes en base a su fila columna, asi, solo 
-		mandariamos en este parametro, por ejemplo, "01","11", ...)
-		tipoElemento: se refiere al tipo de nodo elemeneto que se va a crear, por ejemplo "img","tr","td","h1",...
-	Parametros de retorno: una variable que hace referencia al elemento que se acaba de crear en la funcion. 
+		IdentificadorPadre: Es el identificador del nodo padre al que se le creara un nodo hijo de tipo "texto".
+		texto:es el texto que formara parte del nodo texto. 
 */
 function creaElementoTexto(identificadorPadre,texto)
 {  
