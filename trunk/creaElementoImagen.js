cdbc374a45938bcb1ef5d33926d211535179d2af
rodolfo010385 <rodolfo010385@54d1e357-2446-0410-accf-077c31a16dfc>
2008-02-19 23:07:11 +0000

    se modificaron estos dos archivos para crear la funcion creaElemento

--- trunk/creaElementoImagen.js
+++ trunk/creaElementoImagen.js
@@ -2,7 +2,7 @@
 	transformaCadena - proyecto
 	funcion que crea un nodo elemento "img" a un nodo padre que recive como parametro.
 	parametros:
-		padre: Es el nodo padre al que se le creara un nodo hijo de tipo "img".
+		IdentificadorPadre: Es el nodo padre al que se le creara un nodo hijo de tipo "img".
 		identificador:Es el identificador por medio del cual se podra reconocer al nodo "img" que se va a crear.(esto 
 		por lo que habiamos quedado de que ibamos a identificar las imagenes en base a su fila columna, asi, solo 
 		mandariamos en este parametro, por ejemplo, "01","11", ...)
@@ -10,13 +10,10 @@
 		alto: Hace referencia a la altura que tendra la imagen del nodo hijo que se esta creando.
 		srcc: Hace referencia al atributo source de la imagen.  
 */
-function creaElementoImagen(padre,identificador,ancho,alto,srcc)
+function creaElementoImagen(identificadorPadre,identificador,ancho,alto,srcc)
 {  
-   var imagen=document.createElement("img");
+   var imagen = creaElemento(identificadorPadre,identificador,"img");
    imagen.width=ancho;
    imagen.height=alto;
-   imagen.src=srcc;
-   padre.appendChild(imagen);
-   imagen.setAttribute("id",identificador);  
-   creaAtributoElemento(identificador,"class","mandemama");
+   imagen.src=srcc;  
 }
