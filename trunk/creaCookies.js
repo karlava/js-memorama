eec60227d8bc00d26973c8600cbc5a6e1f9f6b33
rodolfo010385 <rodolfo010385@54d1e357-2446-0410-accf-077c31a16dfc>
2008-02-24 02:43:36 +0000

    se quitaron desplegados

--- trunk/creaCookies.js
+++ trunk/creaCookies.js
@@ -5,15 +5,14 @@
 
 function creaCookies()
 {
- document.cookie = null; 
- var cook = document.cookie;
- 
- var parametros;
- //alert("..........." + cook + "sss");
- if(cook==null || cook=="")
- {
-     parametros = agregaParametro("cecy=1000;patypaletas=900;carmina=800;chaps=700;","sampayo","600",";");
-     document.cookie = escape(parametros);
-     //alert("crea cookie" + unescape(document.cookie));  
- }  
+	 var cook = document.cookie;
+	 var parametros;
+	 if(cook==null || cook=="")
+	 {
+		  var fecha = new Date();
+        fecha.setDate(fecha.getDate() + 30);
+        fecha = fecha.toGMTString();
+	     parametros = agregaParametro("cecy=1000;patypaletas=900;carmina=800;chaps=700;","sampayo","600",";");
+	     document.cookie = escape(parametros+ "expires="+fecha); 
+	 }  
 }
